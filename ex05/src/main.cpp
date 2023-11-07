15723eb9025d3350d64222a3f5f87ccaff5d432b
Luciefer <31387342+lucienfer@users.noreply.github.com>
2023-11-07 12:28:20 +0000

    ex05

---
+++ ex05/src/main.cpp
@@ -0,0 +1,11 @@
+#include "../Harl.hpp"
+
+int main(void) {
+    Harl    Harl;
+
+    Harl.complain("0");
+    Harl.complain("1");
+    Harl.complain("2");
+    Harl.complain("3");
+    return 0;
+}
